4e8129f89ed599a2fd170c881134e6346dd08068
Ashhad <mianashhad7@gmail.com>
2024-06-29 03:22:35 +0500

    Added capibility to crawl each book's web page to extract more info

--- BS4/Books_Data_Scraper.py
+++ BS4/Books_Data_Scraper.py
@@ -2,21 +2,59 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-with open('books.csv', 'w') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Title', 'Price in Pounds', 'Availability'])
+def Save_Row_To_CSV(list_of_values):
+    with open('books.csv', 'a') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(list_of_values)
 
-    source = requests.get('https://books.toscrape.com/').text
+Save_Row_To_CSV(['Title', 'Price in Pounds', 'Availability Status', 'Available Books', 'Rating'])
 
-    soup = BeautifulSoup(source, 'lxml')
+base_url = 'https://books.toscrape.com/'
 
-    products = soup.find_all('article', class_='product_pod')
+source = requests.get(base_url).text
 
-    for product in products:
-        title = product.h3.a['title']
+soup = BeautifulSoup(source, 'lxml')
 
-        price = product.find('div', class_='product_price').find('p', class_='price_color').text.replace('Â£', '')
+# Indicates the source code of a single product on the main page
+products = soup.find_all('article', class_ = 'product_pod')
 
-        availability = product.find('div', class_='product_price').find('p', class_='instock availability').text.strip()
+for product in products:
+    book_href = product.find('h3').a['href']
 
-        csv_writer.writerow([title, price, availability])
+    book_url = base_url + book_href
+
+    book_source = requests.get(book_url).text
+
+    book_soup = BeautifulSoup(book_source, 'lxml')
+
+    # Indicates the source code of the product details section
+    product_details = book_soup.find('div', class_ = 'col-sm-6 product_main')
+
+    book_title = product_details.find('h1').text
+
+    # Strips the currency sign from the price after extracting it
+    book_price_in_pounds = product_details.find('p', class_ = 'price_color').text.replace('Â£', '')
+
+    book_availability = product_details.find('p', class_ = 'instock availability').text.strip()
+
+    # Meant to extract individual values from the string of form "In Stock (22 available)"
+    book_availability_status = book_availability.split('(')[0].strip()
+    number_of_available_books = book_availability.split('(')[1].split()[0]
+
+    # Returns rating in the form "One", "Two"
+    # The format of the class name is "book-rating One" and so on
+    # BS4 is interpreting "book-rating One" as two separate and returning a list of two elements
+    # We need to use find_all becuase this class name varies between books having different ratings
+    book_rating_string = product_details.find_all('p')[2]['class'][1]
+
+    string_to_int = {
+        'One': 1,
+        'Two': 2,
+        'Three': 3,
+        'Four': 4,
+        'Five': 5
+    }
+
+    book_rating_int = string_to_int.get(book_rating_string, None)
+
+    Save_Row_To_CSV([book_title, book_price_in_pounds, book_availability_status, number_of_available_books, book_rating_int])
