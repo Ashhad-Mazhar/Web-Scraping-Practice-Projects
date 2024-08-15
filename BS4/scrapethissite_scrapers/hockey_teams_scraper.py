a577db37a376880b34f5763cf15b8e11818658e4
Ashhad <mianashhad7@gmail.com>
2024-08-15 05:19:51 +0500

    Scraped all pages of the hockey teams site and improved the coding style

--- BS4/scrapethissite_scrapers/hockey_teams_scraper.py
+++ BS4/scrapethissite_scrapers/hockey_teams_scraper.py
@@ -1,14 +1,55 @@
+import time
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
+
+def check_page_validity(soup_object: BeautifulSoup) -> bool:
+    '''
+    Checks to see if the given page has any team entries in the
+    table. No entries in the table other than the header row means
+    that the page is invalid.
+    '''
+    data_table = soup_object.find('table', {'class': 'table'})
+    if data_table.find('tr', {'class': 'team'}):
+        return True
+    else:
+        return False
+    
+
+def get_all_page_soups(base_url: str) -> list:
+    '''
+    Returns BeautifulSoup objects for all pages by iterating through
+    page numbers until the program reaches the last page
+    '''
+    page_number = 1
+    soup_objects = []
+    while True:
+        response = requests.get(base_url, params={'page_num': page_number})
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        if check_page_validity(soup):
+            soup_objects.append(soup)
+        else:
+            break
+        page_number += 1
+    return soup_objects
 
 def get_specific_info(tag_object, tag_name, *possible_tag_classes) -> str:
+    '''
+    Returns a single piece of required info after parsing a single
+    team record. Checks multiple class names in case of a possibilty
+    of different class names for the same tag in the page.
+    '''
     for tag_class in possible_tag_classes:
         info = tag_object.find(tag_name, {'class': tag_class})
         if info:
             return info.get_text().strip()
 
 def get_team_info(tag_object) -> dict:
+    '''
+    Returns a dictionary representing one complete record in the page
+    table.
+    '''
     team_name = get_specific_info(tag_object, 'td', 'name')
     year = get_specific_info(tag_object, 'td', 'year')
     wins = get_specific_info(tag_object, 'td', 'wins')
@@ -35,26 +76,28 @@ def get_team_info(tag_object) -> dict:
     }
     return data
 
+# Used to measure the time taken by the program to execute
+start_time = time.time()
 
 BASE_URL = 'https://www.scrapethissite.com/pages/forms/'
-
-response = requests.get(BASE_URL, params={'page_num': 1})
-html = response.content
-soup = BeautifulSoup(html, 'html.parser')
-
-data_table = soup.find('table', {'class': 'table'})
-teams = data_table.find_all('tr', {'class': 'team'})
 
 team_infos = []
 teams_collected = 0
 
-for team in teams:
-    team_info = get_team_info(team)
-    team_infos.append(team_info)
-    teams_collected += 1
-    print(teams_collected)
+soups = get_all_page_soups(BASE_URL)
+
+for soup in soups:
+    data_table = soup.find('table', {'class': 'table'})
+    teams = data_table.find_all('tr', {'class': 'team'})
+    for team in teams:
+        team_info = get_team_info(team)
+        team_infos.append(team_info)
+        teams_collected += 1
+        print(teams_collected)
 
 df = pd.DataFrame.from_dict(team_infos)
 df.index = range(1, len(df) + 1)
 df.index.name = 'record_id'
 df.to_csv('teams.csv', index=True)
+
+print(f'--- {time.time() - start_time} seconds ---')
