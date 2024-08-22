5e62d6d9a40bee53091053c724d648e1a68184f0
Ashhad <mianashhad7@gmail.com>
2024-08-22 06:51:01 +0500

    Wrote a helper function for parse_row() in place of multiple try catch blocks

--- BS4/football_data_scrapers/transfermarkt_scrapers/top_transfers_scraper/parser.py
+++ BS4/football_data_scrapers/transfermarkt_scrapers/top_transfers_scraper/parser.py
@@ -16,6 +16,23 @@ class TransferParser:
             all_data.append(player_data)
         return all_data
     
+    def safe_extract(
+            self,
+            function: callable,
+            error_message: str,
+            default_value:str='MISSING VALUE'
+    ) -> str:
+        '''
+        Executes the given function and returns its result. In
+        case of an expection, returns the default value and prints
+        an error message to the terminal
+        '''
+        try:
+            return function()
+        except Exception as e:
+            print(error_message)
+            return default_value
+
     def parse_row(self, row: BeautifulSoup) -> dict:
         '''
         Returns a complete record of the transfer after parsing
@@ -32,82 +49,64 @@ class TransferParser:
         transfer_fee = cells[8]
 
         transfer_data = {}
-        try:
-            transfer_data['player_name'] = (
-                name_position_image.find('a').get_text().strip()
-            )
-        except:
-            transfer_data['player_name'] = 'MISSING VALUE'
-            print('Could not retrieve player name')
-        try:
-            transfer_data['player_page_url'] = (
-                BASE_URL + name_position_image.find('a')['href']
-            )
-        except:
-            transfer_data['player_page_url'] = 'MISSING VALUE'
-            print('Could not retrieve player page URL')
-        try:
-            transfer_data['player_image_url'] = (
-                name_position_image.find('img')['data-src']
-            )
-        except:
-            transfer_data['player_image_url'] = 'MISSING VALUE'
-            print('Could not retrieve player image URL')
-        try:
-            transfer_data['player_position'] = (
-                name_position_image.find_all('tr')[1].get_text().strip()
-            )
-        except:
-            transfer_data['player_position'] = 'MISSING VALUE'
-            print('Could not retrieve player position')
-        try:
-            transfer_data['player_age'] = age.get_text().strip()
-        except:
-            transfer_data['player_age'] = 'MISSING VALUE'
-            print('Could not retrieve player age')
-        try:
-            transfer_data['player_value_in_euros'] = market_value.get_text().strip()
-        except:
-            transfer_data['player_value_in_euros'] = 'MISSING VALUE'
-            print('Could not retrieve player value')
-        try:
-            transfer_data['season'] = season.find('a').get_text().strip()
-        except:
-            transfer_data['season'] = 'MISSING VALUE'
-            print('Could not retrieve season')
-        try:
-            nationalities = [
-                country['title'] for country in nationality.find_all('img')
-            ]
-            transfer_data['player_nationalities'] = nationalities
-        except:
-            transfer_data['player_nationalities'] = 'MISSING VALUE'
-            print('Could not retrieve player nationalities')
-        try:
-            old_club_info = old_club.find_all('tr')
-            transfer_data['old_club_name'] = old_club_info[0].find('a')['title']
-        except:
-            transfer_data['old_club_name'] = 'MISSING VALUE'
-            print('Could not retrieve old club info')
-        try:
-            transfer_data['old_league_name'] = old_club_info[1].find('a')['title']
-        except:
-            transfer_data['old_league_name'] = 'MISSING VALUE'
-            print('Could not retrieve old club info')
-        try:
-            new_club_info = new_club.find_all('tr')
-            transfer_data['new_club_name'] = new_club_info[0].find('a')['title']
-        except:
-            transfer_data['new_club_name'] = 'MISSING VALUE'
-            print('Could not retrieve new club info')
-        try:
-            transfer_data['new_league_name'] = new_club_info[1].find('a')['title']
-        except:
-            transfer_data['new_league_name'] = 'MISSING VALUE'
-            print('Could not retrieve new club info')
-        try:
-            transfer_data['transfer_fee_in_euros'] = transfer_fee.find('a').get_text().strip()
-        except:
-            transfer_data['transfer_fee_in_euros'] = 'MISSING VALUE'
-            print('Could not retrieve transfer fee')
+        transfer_data['player_name'] = self.safe_extract(
+            lambda: name_position_image.find('a').get_text().strip(),
+            'Could not retrieve player name'
+        )
+        transfer_data['player_page_url'] = self.safe_extract(
+            lambda: BASE_URL + name_position_image.find('a')['href'],
+            'Could not retrieve player page URL'
+        )
+        transfer_data['player_image_url'] = self.safe_extract(
+            lambda: name_position_image.find('img')['data-src'],
+            'Could not retrieve player image URL'
+        )
+        transfer_data['player_position'] = self.safe_extract(
+            lambda: name_position_image.find_all('tr')[1].get_text().strip(),
+            'Could not retrieve player position'
+        )
+        transfer_data['player_age'] = self.safe_extract(
+            lambda: age.get_text().strip(),
+            'Could not retrieve player age'
+        )
+        transfer_data['player_value_in_euros'] = self.safe_extract(
+            lambda: market_value.get_text().strip(),
+            'Could not retrieve player value'
+        )
+        transfer_data['season'] = self.safe_extract(
+            lambda: season.find('a').get_text().strip(),
+            'Could not retrieve transfer season'
+        )
+        transfer_data['player_nationalities'] = self.safe_extract(
+            lambda: [country['title'] for country in nationality.find_all('img')],
+            'Could not retrieve player nationalities'
+        )
+        old_club_info = self.safe_extract(
+            lambda: old_club.find_all('tr'),
+            'Could not retrieve old club data'
+        )
+        transfer_data['old_club_name'] = self.safe_extract(
+            lambda: old_club_info[0].find('a')['title'],
+            'Could not retrieve old club name'
+        )
+        transfer_data['old_league_name'] = self.safe_extract(
+            lambda: old_club_info[1].find('a')['title'],
+            'Could not retrieve old league name'
+        )
+        new_club_info = self.safe_extract(
+            lambda: new_club.find_all('tr'),
+            'Could not retrieve new club info'
+        )
+        transfer_data['new_club_name'] = self.safe_extract(
+            lambda: new_club_info[0].find('a')['title'],
+            'Could not retrieve new club name'
+        )
+        transfer_data['new_league_name'] = self.safe_extract(
+            lambda: new_club_info[1].find('a')['title'],
+            'Could not retrieve new league name'
+        )
+        transfer_data['transfer_fee_in_euros'] = self.safe_extract(
+            lambda: transfer_fee.find('a').get_text().strip(),
+            'Could not retrieve transfer fee'
+        )
         return transfer_data
